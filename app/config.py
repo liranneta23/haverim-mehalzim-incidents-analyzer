164b68fa532575efc1292a130e14044ef1c0e369
Liran <netaliran23@gmail.com>
2026-05-10 18:20:57 +0200

    fix small bug

--- app/config.py
+++ app/config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOARD_ID          = 1783313577
+BOARD_ID          = os.getenv("BOARD_ID")
 MONDAY_URL        = "https://api.monday.com/v2"
 API_KEY           = os.getenv("MONDAY_API_KEY")
 FEEDBACK_BOARD_ID = os.getenv("FEEDBACK_BOARD_ID")
-DONORS_BOARD_ID   = os.getenv("DONORS_BOARD_ID") or "2027127204"
+DONORS_BOARD_ID   = os.getenv("DONORS_BOARD_ID")
 ADMIN_TOKEN       = os.getenv("ADMIN_TOKEN")
 
 MONDAY_HEADERS = {
